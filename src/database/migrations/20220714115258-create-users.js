b3630b37fa07d3e6a7576a6cef9de4f8cd3cdb2f
Andre Felipe <afelipes@gmail.com>
2022-07-14 10:13:08 -0300

    feat: :rocket: 1 - Crie migrations para as entidades User, Categories, BlogPosts, PostCategories

---
+++ src/database/migrations/20220714115258-create-users.js
@@ -0,0 +1,45 @@
+// https://stackoverflow.com/questions/28414395/how-to-update-with-sequelize-with-now-on-a-timestamp
+
+'use strict';
+module.exports = {
+    /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};

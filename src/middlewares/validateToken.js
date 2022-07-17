6aa6047533708dd6db82965d332776e77fd677bb
Andre Felipe <afelipes@gmail.com>
2022-07-17 18:59:06 -0300

    feat: Requisito 5
    Sua aplicação deve ter o endpoint GET /user
    
    Retirei a função que limpava o token retirando a string  "Bearer " para passar no requisito

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,17 +1,18 @@
 const authService = require('../services/authService');
 const { throwUnauthorizedError } = require('../services/utils');
 
-const clearToken = (data) => { // função para deixar só o token
-  const [, token] = data.split(' ');
-  return token;
-};
+// NÃO USAR O QUE ERA IMPORTANTE 
+// const clearToken = (data) => { // função para deixar só o token
+//   const [, token] = data.split(' ');
+//   return token;
+// };
 
 const validateToken = async (req, _res, next) => {
   const token = req.headers.authorization;
     if (!token) throwUnauthorizedError('Token not found');
-  const newToken = clearToken(token);
+  // const newToken = clearToken(token);
   try {
-    await authService.readToken(newToken);
+    await authService.readToken(token);
   } catch (error) {
     throwUnauthorizedError('Expired or invalid token');
   }
